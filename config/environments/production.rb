9d742c1a4e8979198944b16e5d40d7ff3d1e7525
Ant <alonezuko@mail.ru>
2015-04-22 13:00:53 +0600

    dance with frontend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,18 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
+# Disable Rails's static asset server (Apache or nginx will already do this)
+
+  config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
